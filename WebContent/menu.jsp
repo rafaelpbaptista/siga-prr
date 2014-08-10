5bb30e80ba26977105c95e442f5fd515d1917730
rafaelpbaptista <rafael.baptista@caelum.com.br>
2014-08-10 00:04:54 -0300

    [SIGA-PRR] Pagina de login

---
+++ WebContent/menu.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>SIGA-PRR</title>
+<link rel="stylesheet" href="css/estilos.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css"
+	href="http://fonts.googleapis.com/css?family=Play">
+<script src="//barra.brasil.gov.br/barra.js" type="text/javascript"></script>
+</head>
+<body>
+	<c:import url="cabecalho.jspf" />
+	<c:import url="rodape.jspf" />
+</body>
+</html>
---
+++ src/br/edu/ifrj/siga/prr/modelo/Usuario.java
@@ -0,0 +1,45 @@
+package br.edu.ifrj.siga.prr.modelo;
+
+import java.util.List;
+
+public class Usuario {
+
+	private Long id;
+	private String login;
+	private String senha;
+
+	private List<Grupo> grupos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+
+}
